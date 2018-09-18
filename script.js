f0cb47766fd1e2a2d6ab5f3f3f7c52a29ed6807f
CasperEhlin <casper.ehlin@hotmail.se>
2018-09-18 09:29:58 +0200

    Färdigställda uträkningar

--- script.js
+++ script.js
@@ -4,20 +4,6 @@ $(document).ready(function(){
 
 $("input").change(function(){  
 
-  var elev1total=0;
-  var elev2total=0;
-  var elev3total=0;
-  var elev4total=0;
-  var elev5total=0;
-
-  var testmax;
-
-  var task1max=0;
-    var task2max=0;
-      var task3max=0;
-        var task4max=0;
-          var task5max=0;
-/*
   let task1avg=$(".provsvar div:nth-child(1) .answer"), task1Total=0,task1Average=0;
   for(let i=0;i<task1avg.length;i++)
              {task1Total+= parseFloat(task1avg[i].value)}// lägger samman värdena i arrayen
@@ -30,28 +16,29 @@ $("input").change(function(){
        task2Average=(task2Total/task2avg.length);// räknar ut medelvärde
           console.log(task2Average); // Visar resultat (temporär för att kunna se så att detta funkar på fulsidan)
 
-          let task2avg=$(".provsvar div:nth-child(3) .answer"), task2Total=0,task2Average=0;
-          for(var i=0;i<task3avg.length;i++)
-             {task3Total+= parseFloat(task3avg[i])}// lägger samman värdena i arrayen
+          let task3avg=$(".provsvar div:nth-child(3) .answer"), task3Total=0,task3Average=0;
+          for(let i=0;i<task3avg.length;i++)
+             {task3Total+= parseFloat(task3avg[i].value)}// lägger samman värdena i arrayen
                task3Average=(task3Total/task3avg.length);// räknar ut medelvärde
                   console.log(task3Average); // Visar resultat (temporär för att kunna se så att detta funkar på fulsidan)
 
-                  let task2avg=$(".provsvar div:nth-child(4) .answer"), task2Total=0,task2Average=0;
-                  for(var i=0;i<task4avg.length;i++)
-                     {task4Total+= parseFloat(task4avg[i])}// lägger samman värdena i arrayen
+                  let task4avg=$(".provsvar div:nth-child(4) .answer"), task4Total=0,task4Average=0;
+                  for(let i=0;i<task4avg.length;i++)
+                     {task4Total+= parseFloat(task4avg[i].value)}// lägger samman värdena i arrayen
                        task4Average=(task4Total/task4avg.length);// räknar ut medelvärde
                           console.log(task4Average); // Visar resultat (temporär för att kunna se så att detta funkar på fulsidan)
 
-                          let task2avg=$(".provsvar div:nth-child(5) .answer"), task2Total=0,task2Average=0;
-                            for(var i=0;i<task5avg.length;i++)
-                              {task5Total+= parseFloat(task5avg[i])}// lägger samman värdena i arrayen
+                          let task5avg=$(".provsvar div:nth-child(5) .answer"), task5Total=0,task5Average=0;
+                            for(let i=0;i<task5avg.length;i++)
+                              {task5Total+= parseFloat(task5avg[i].value)}// lägger samman värdena i arrayen
                                 task5Average=(task5Total/task5avg.length);// räknar ut medelvärde
                                     console.log(task5Average); // Visar resultat (temporär för att kunna se så att detta funkar på fulsidan)
-  */
-                                    let klassavg=$(""), klassTotal=0,klassAverage=0;
+ 
+                                    let klassavg=$(".totalpoint"), klassTotal=0,klassAverage=0;
                                     for(let i=0;i<klassavg.length;i++)
-                                     {klassTotal+= parseFloat(klassavg[i])}// lägger samman värdena i arrayen
+                                     {klassTotal+= parseFloat(klassavg[i].value)}// lägger samman värdena i arrayen
                                        klassAverage=(klassTotal/klassavg.length);// räknar ut medelvärde
                                            console.log(klassAverage); // Visar resultat (temporär för att kunna se så att detta funkar på fulsidan)
+                                           
 });
 });
